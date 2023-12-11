7ca37994abfc5e615adb997d3cfee54acd131fd1
Adrien Vannson <adrien.vannson@protonmail.com>
2023-12-11 15:19:59 +0000

    Three linear layers

--- eval.sh
+++ eval.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Only one linear layer
 python run.py exp.name=relationnet_finetune_swissprot run.name=layers-1_dropout-00 method=relationnet dataset=swissprot method.cls.layers_size=[1] method.cls.dropout=0.0
 python run.py exp.name=relationnet_finetune_swissprot run.name=layers-1_dropout-20 method=relationnet dataset=swissprot method.cls.layers_size=[1] method.cls.dropout=0.2
@@ -19,3 +21,16 @@ python run.py exp.name=relationnet_finetune_swissprot run.name=layers-128-1_drop
 python run.py exp.name=relationnet_finetune_swissprot run.name=layers-512-1_dropout-00 method=relationnet dataset=swissprot method.cls.layers_size=[512,1] method.cls.dropout=0.0
 python run.py exp.name=relationnet_finetune_swissprot run.name=layers-512-1_dropout-20 method=relationnet dataset=swissprot method.cls.layers_size=[512,1] method.cls.dropout=0.2
 python run.py exp.name=relationnet_finetune_swissprot run.name=layers-512-1_dropout-50 method=relationnet dataset=swissprot method.cls.layers_size=[512,1] method.cls.dropout=0.5
+
+# Three linear layers
+python run.py exp.name=relationnet_finetune_swissprot run.name=layers-128-32-1_dropout-00 method=relationnet dataset=swissprot method.cls.layers_size=[128,32,1] method.cls.dropout=0.0
+python run.py exp.name=relationnet_finetune_swissprot run.name=layers-128-32-1_dropout-20 method=relationnet dataset=swissprot method.cls.layers_size=[128,32,1] method.cls.dropout=0.2
+python run.py exp.name=relationnet_finetune_swissprot run.name=layers-128-32-1_dropout-50 method=relationnet dataset=swissprot method.cls.layers_size=[128,32,1] method.cls.dropout=0.5
+
+python run.py exp.name=relationnet_finetune_swissprot run.name=layers-256-64-1_dropout-00 method=relationnet dataset=swissprot method.cls.layers_size=[256,64,1] method.cls.dropout=0.0
+python run.py exp.name=relationnet_finetune_swissprot run.name=layers-256-64-1_dropout-20 method=relationnet dataset=swissprot method.cls.layers_size=[256,64,1] method.cls.dropout=0.2
+python run.py exp.name=relationnet_finetune_swissprot run.name=layers-256-64-1_dropout-50 method=relationnet dataset=swissprot method.cls.layers_size=[256,64,1] method.cls.dropout=0.5
+
+python run.py exp.name=relationnet_finetune_swissprot run.name=layers-512-128-1_dropout-00 method=relationnet dataset=swissprot method.cls.layers_size=[512,128,1] method.cls.dropout=0.0
+python run.py exp.name=relationnet_finetune_swissprot run.name=layers-512-128-1_dropout-20 method=relationnet dataset=swissprot method.cls.layers_size=[512,128,1] method.cls.dropout=0.2
+python run.py exp.name=relationnet_finetune_swissprot run.name=layers-512-128-1_dropout-50 method=relationnet dataset=swissprot method.cls.layers_size=[512,128,1] method.cls.dropout=0.5
